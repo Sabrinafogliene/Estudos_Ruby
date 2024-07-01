66947d19b97ba24b81bd96c532b14e963582bf83
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-01 19:52:12 -0300

    Create aula2.rb

---
+++ aula2.rb
@@ -0,0 +1,18 @@
+#1)Uma saída de dados na tela --> aparecer um comando na tela.
+# usuário digite seu nome.
+print"Digite seu nome: "
+
+
+
+
+
+#2)programa permita que o usuário preencha o que foi pedido
+#método gets.chomp
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+#3)saída final
+#para chamar um dado #{variável}
+puts "Oi, #{nome} #{sobrenome}!"
