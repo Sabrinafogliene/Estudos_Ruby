c09f77042ad9de22def284336d7601d5f9b091c6
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-02 17:14:18 -0300

    Create else.rb

---
+++ else.rb
@@ -0,0 +1,11 @@
+#analise um dia da semana
+#SE esse dia for domingo
+#IMPRIMA que o nosso almoço será especial
+dia = 'domingo'
+if dia == 'domingo' #== é uma comparação
+    almoco = 'especial'
+else
+    almoco = 'normal'
+end
+#imprime --> puts
+puts "Hoje nosso almoço será #{almoco}!"
