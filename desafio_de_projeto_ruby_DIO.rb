129ee65c3c791a7416f179b5323c20dbb1c852c1
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-02 16:40:08 -0300

    Resolução_Desafio_de_Projeto_DIO

---
+++ desafio_de_projeto_ruby_DIO.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite a sua idade: "
+idade = gets.chomp
+
+puts "O seu nome é #{nome} #{sobrenome} e você tem #{idade} anos. "
