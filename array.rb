7140d8acbcaef3a56f794e3232640286889fe2c9
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-04 12:32:38 -0300

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,25 @@
+#vamos manipular arrays
+#lista de livros
+#criar um array vazio
+
+#vamos adicionar um item nesse array nome.push('o que queremos colocar')
+
+#vamos adicionar vários item ao nosso array ao mesmo tempo
+#nome.push('O que queremos colocar', 'O que queremos colocar 2')
+
+#organizando os novos dados da lista nome.insert(0, 'nome1', 'nome2')
+
+#acessando os elementos do array nome[numero]
+#acessando intervalos do array nome[1..4]
+#recuperar o primeiro item nome.first
+#recuperar o último item nome.last
+
+#para saber quantidade de elementos de um array nome.count ou nome.length
+
+#descubra se o array está vazio nome.empty?
+
+#verifique se um nome está presente nesse array nome.include?('nome')
+#para excluir elementos:
+#elemento específico nome.delete_at(numero)
+#exclua o último elemento nome.pop
+#exclua o primeiro elemento nome.shift
