79412b9b555d212f281ab6d16dbf37c742e350ab
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-09 14:46:06 -0300

    Create escopo.rb

---
+++ escopo.rb
@@ -0,0 +1,22 @@
+#escopo: define onde a variável estará disponível dentro do seu programa.
+#4 tipos em ruby:
+#1. Locais
+#2. Globais
+#3. Classes
+#4. Instância
+
+#LOCAIS: Podem ser acessadas apenas onde foram criadas.
+#Forma: primeira letra minuscula ou sublinhada
+
+
+
+
+#GLOBAIS: Pode ser acessada de qualquer lugar do programa.
+#forma: use o prefixo $
+#USO DESENCORAJADO
+
+#CLASSE: Pode ser acessada de qualquer lugar da classe.
+#Forma: @@
+
+#INSTÂNCIA: Semelhante a de classe
+#Forma: @
