15c838c2fdb0207d741b54c5baafc8dd94a12bb3
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-09 14:46:43 -0300

    Create instancia.rb

---
+++ instancia.rb
@@ -0,0 +1,16 @@
+#Bem usadas
+
+class Usuario
+    def add(nome)
+        @nome = nome
+        puts "Usuário adicionado"
+        ola
+    end 
+
+    def ola
+        puts "Seja bem vindo(a), #{@nome}!"
+    end
+end
+
+usuario = Usuario.new
+usuario.add('Sabrina')
