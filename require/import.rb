0edd695442ba10616fafba2f202a6d75efc6f5e9
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-09 12:58:44 -0300

    Create import.rb

---
+++ require/import.rb
@@ -0,0 +1,6 @@
+require_relative './animal.rb'
+require_relative './gato.rb'
+
+#require_relative './animal.rb' - na hr de chamar o arquivo chama a pasta dele junto. Ex: ruby require/import.rb
+
+#require './animal.rb' - nesse caso tem que abrir a pasta q ele ta no terminal. Aí coloca cd e o nome da pasta e qndo abrir coloca ruby import.rb
