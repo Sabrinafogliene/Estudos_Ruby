46dcc96f77be0af79ee7828d1bc2a85e595021b7
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-07-09 14:46:32 -0300

    Create class.rb

---
+++ class.rb
@@ -0,0 +1,17 @@
+#Pouco usadas
+
+
+class Usuario
+    @@usuario_count = 0
+    def add(name)
+        puts "Usuário #{name} adicionado"
+        @@usuario_count +=1
+        puts @@usuario_count
+    end
+end
+
+first_user = Usuario.new
+first_user.add('Sabrina')
+
+second_user = Usuario.new
+second_user.add('Paulo')
